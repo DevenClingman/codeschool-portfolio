72d9b52783f0f0cce98a63ef4fbf1120d5cde1f1
Deven Clingman <dclingy@gmail.com>
2017-07-31 11:44:45 -0600

    integrated new gem into rails app for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,4 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
-
-  before_action :set_copyright
-
-  def set_copyright
-    @copyright = ClingmanViewTool::Renderer.copyright "Deven Clingman", "All rights reserved"
-  end
 end
-
